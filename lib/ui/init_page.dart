ecb0b63b9a87d24aef392ea951fbed09473e6100
jeong jun <wjdwns3744@naver.com>
2023-06-04 01:25:59 +0900

    init 페이지 기본화면 작성

--- lib/ui/init_page.dart
+++ lib/ui/init_page.dart
@@ -1,6 +1,10 @@
 import 'package:fairerweb/config/app_color.dart';
+import 'package:fairerweb/ui/widgets/cta_button.dart';
+import 'package:fairerweb/ui/widgets/init_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
 class InitPage extends StatelessWidget {
   @override
@@ -8,9 +12,42 @@ class InitPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
+          width: Get.width,
+          height: Get.height,
+          padding: const EdgeInsets.symmetric(vertical: 24,horizontal: 24),
           color: Palette.fairerBlue,
           child: Column(
-            children: [Align()],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                height: 4,
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    "icons/ic_fairer.svg",
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      Text("init_title".tr,style: const TextStyle(fontSize: 44,color: Palette.white),textAlign: TextAlign.center,),
+                      Text("init_question".tr,style: const TextStyle(fontSize: 18,color: Palette.white),textAlign: TextAlign.center,)
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  child: InitButton()
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/ui/widgets/init_button.dart
+++ lib/ui/widgets/init_button.dart
@@ -6,7 +6,7 @@ class InitButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Palette.fairerBlue,
+      color: Palette.white,
       width: Get.width,
       height: 56,
       child: Center(
