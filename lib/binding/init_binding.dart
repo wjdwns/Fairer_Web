50ce34dc75b5ac57bbc138919d37bfbe2c3600b0
jeong jun <wjdwns3744@naver.com>
2023-06-04 01:36:22 +0900

    question페이지 생성, result페이지 생성

--- lib/binding/init_binding.dart
+++ lib/binding/init_binding.dart
@@ -1,8 +1,9 @@
+import 'package:fairerweb/controller/global_controller.dart';
 import 'package:get/get.dart';
 
 class InitBinding extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.put(GlobalController(),permanent: true);
   }
 }
---
+++ lib/controller/global_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class GlobalController extends GetxController{
+  final page = 0.obs;
+  
+}
