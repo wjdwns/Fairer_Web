51aa6c70e5747eb6f6a71d6b7d1fd06454940f21
jeong jun <wjdwns3744@naver.com>
2023-06-08 21:31:00 +0900

    색 블랙으로 변경

--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -11,7 +11,7 @@ class CustomTextStyle {
   static const nanum = 'NanumSquareNeoOTF';
 
   static const H1 = TextStyle(fontFamily: Bold, fontSize: 24, letterSpacing: (-2), height: 1.4);
-  static const H2 = TextStyle(fontFamily: Bold, fontSize: 20, letterSpacing: (-2), height: 1.4);
+  static const H2 = TextStyle(fontFamily: Bold, fontSize: 20, letterSpacing: (-2), height: 1.4,color: Palette.black);
   static const H3 = TextStyle(fontFamily: Medium, fontSize: 20, letterSpacing: (-2), height: 1.4);
   static const Title1 = TextStyle(fontFamily: SemiBold, fontSize: 16, height: 1.4);
   static const Title2 = TextStyle(fontFamily: SemiBold, fontSize: 14, height: 1.4);
